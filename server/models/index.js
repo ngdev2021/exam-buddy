17bf325d2575c8d57fba96a4fecc12577f47f3a9
ngdev2021 <ngdev21@gmail.com>
2025-04-20 15:06:20 -0500

    Fix: Accept self-signed certs for AWS RDS in Sequelize

--- server/models/index.js
+++ server/models/index.js
@@ -5,6 +5,12 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 export default sequelize;
